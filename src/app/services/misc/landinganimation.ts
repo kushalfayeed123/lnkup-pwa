acf54b7dca0951d6a82234596afe5a5d1a42b6a6
Segun Ajanaku <sajanaku@eminenttechnology.com>
2019-08-16 20:15:04 +0100

    fix bug

--- src/app/services/misc/landinganimation.ts
+++ src/app/services/misc/landinganimation.ts
@@ -9,11 +9,25 @@ export const popInAnimation =
         transition(':enter', [
 
             // css styles at start of transition
-            style({ transform: 'scale(5)', opacity: 0.2}),
+            style({ transform: 'translateY(-100px)', opacity: 0}),
 
             // animation and styles at end of transition
-            animate('1200ms  cubic-bezier(0.55, 0.055, 0.675, 0.19)', style({ opacity: 0.2,
+            animate('2s  cubic-bezier(0.55, 0.055, 0.675, 0.19)', style({ opacity: 0.2,
               background: '#D73872',
+              transform: 'translateY(-100px)',
+
              }))
         ]),
+        transition(':leave', [
+
+          // css styles at start of transition
+          style({ transform: 'translateY(-100px)', opacity: 0}),
+
+          // animation and styles at end of transition
+          animate('2s  cubic-bezier(0.55, 0.055, 0.675, 0.19)', style({ opacity: 0.2,
+            background: '#black',
+            transform: 'translateY(-100px)',
+
+           }))
+      ]),
     ]);
