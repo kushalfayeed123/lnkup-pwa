f16c88ee9088c871487fda76293819d7fff921fa
King Kush <segunajanaku617@gmail.com>
2019-10-01 00:20:09 +0100

    fix bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  webUrl : 'https://linkup20190824020415.azurewebsites.net/api'
+  webUrl : 'https://linkup20190824020415.azurewebsites.net/api',
+  openConnect: 'https://linkup20190824020415.azurewebsites.net/notifyHub'
 };
