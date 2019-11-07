7e16ec8210341ccf18cd67e4f2521237c3a92739
King Kush <segunajanaku617@gmail.com>
2019-11-07 07:37:33 +0100

    add prod endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  webUrl : 'https://linkup20190824020415.azurewebsites.net/api',
-  openConnect: 'https://linkup20190824020415.azurewebsites.net/notifyHub'
+  webUrl : 'https://linkup20191021111853.azurewebsites.net/api',
+  openConnect: 'https://linkup20191021111853.azurewebsites.net/notifyHub',
+  vapidPublicKey: 'BCleJ0xgtXBcFHep4RmZXWX3Fa6S6ofO-XCzK56AHW3CXPlXZBsOtQ1ydmoidDBXaPeZdvA70ybSXB6atJBq7Jc',
 };
