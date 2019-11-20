124a614b550b287d7fef6616ab42e8ca2ff0b11a
King Kush <segunajanaku617@gmail.com>
2019-11-20 09:02:50 +0100

    fix driver trip create bug

--- src/app/driver/driver-trip-create/driver-trip-create.component.ts
+++ src/app/driver/driver-trip-create/driver-trip-create.component.ts
@@ -34,9 +34,9 @@ export class DriverTripCreateComponent implements OnInit, OnDestroy {
   dateNow: string;
 
   constructor(private fb: FormBuilder, private mapService: MapBroadcastService,
-              private activeTripService: ActiveTripDataService,
-              private router: Router,
-              private notifyService: NotificationsService) {
+    private activeTripService: ActiveTripDataService,
+    private router: Router,
+    private notifyService: NotificationsService) {
 
   }
 
@@ -46,34 +46,36 @@ export class DriverTripCreateComponent implements OnInit, OnDestroy {
     this.getTimeValues();
     this.fare = 0;
     this.tripForm = this.fb.group({
-      tripPickup: [this.pickup],
-      tripDestination: [this.destination],
-      driverDataId: [this.driverDataId],
-      driverTripStatus: [1],
-      driverStartLongitude: [this.tripStartLng],
-      driverStartLatitude: [this.tripStartLat],
-      driverEndLongitude: [this.tripEndLng],
-      driverEndLatitude: [this.tripEndLat],
+      tripPickup: [this.pickup, [Validators.required]],
+      tripDestination: [this.destination, [Validators.required]],
+      driverDataId: [this.driverDataId, [Validators.required]],
+      driverTripStatus: [1, [Validators.required]],
+      driverStartLongitude: [this.tripStartLng, [Validators.required]],
+      driverStartLatitude: [this.tripStartLat, [Validators.required]],
+      driverEndLongitude: [this.tripEndLng, [Validators.required]],
+      driverEndLatitude: [this.tripEndLat, [Validators.required]],
       maxRiderNumber: ['', [Validators.required]],
       tripStartDateTime: ['', [Validators.required]],
       aggregrateTripFee: [this.fare, [Validators.required]],
       tripType: ['', [Validators.required]],
-      allowedRiderCount: [0],
-      tripConnectionId  : [this.connectionId]
+      allowedRiderCount: [0, [Validators.required]],
+      tripConnectionId: [this.connectionId, [Validators.required]]
     });
   }
 
   getDriverDetails() {
     const driverDataId = localStorage.getItem('driverDataId');
     this.driverDataId = driverDataId;
-    const connectionId = sessionStorage. getItem('clientConnectionId');
+    const connectionId = sessionStorage.getItem('clientConnectionId');
     this.connectionId = connectionId;
   }
 
 
   getLocationCoordinates() {
-      const tripStartLatLng = JSON.parse(localStorage.getItem('origin'));
-      const tripEndLatLng = JSON.parse(localStorage.getItem('destination'));
+    this.loading = true;
+    const tripStartLatLng = JSON.parse(localStorage.getItem('origin'));
+    const tripEndLatLng = JSON.parse(localStorage.getItem('destination'));
+    if (tripEndLatLng !== null && tripStartLatLng !== null) {
       this.tripStartLat = tripStartLatLng.lat.toString();
       this.tripStartLng = tripStartLatLng.lng.toString();
       this.tripEndLat = tripEndLatLng.lat.toString();
@@ -81,25 +83,53 @@ export class DriverTripCreateComponent implements OnInit, OnDestroy {
       const origin = new google.maps.LatLng(tripStartLatLng.lat, tripStartLatLng.lng);
       const destination = new google.maps.LatLng(tripEndLatLng.lat, tripEndLatLng.lng);
 
-      new google.maps.DistanceMatrixService().getDistanceMatrix({origins: [origin], destinations: [destination],
-        travelMode: google.maps.TravelMode.DRIVING}, (results: any) => {
-          this.tripDistance =  (results.rows[0].elements[0].distance.value / 1000);
+      new google.maps.DistanceMatrixService().getDistanceMatrix({
+        origins: [origin], destinations: [destination],
+        travelMode: google.maps.TravelMode.DRIVING
+      }, (results: any) => {
+        this.tripDistance = (results.rows[0].elements[0].distance.value / 1000);
+        this.tripTime = (results.rows[0].elements[0].duration.text);
+        const pricePerRiderPerKm = 37;
+        const tripPricePerRider = Math.round(pricePerRiderPerKm * this.tripDistance);
+        this.tripPricePerRider = tripPricePerRider;
+        console.log('trip price per rider', tripPricePerRider);
+        this.loading = false;
+      });
+    } else {
+      setTimeout(() => {
+        const tripStartLatLng = JSON.parse(localStorage.getItem('origin'));
+        const tripEndLatLng = JSON.parse(localStorage.getItem('destination'));
+        this.tripStartLat = tripStartLatLng.lat.toString();
+        this.tripStartLng = tripStartLatLng.lng.toString();
+        this.tripEndLat = tripEndLatLng.lat.toString();
+        this.tripEndLng = tripEndLatLng.lng.toString();
+        const origin = new google.maps.LatLng(tripStartLatLng.lat, tripStartLatLng.lng);
+        const destination = new google.maps.LatLng(tripEndLatLng.lat, tripEndLatLng.lng);
+
+        new google.maps.DistanceMatrixService().getDistanceMatrix({
+          origins: [origin], destinations: [destination],
+          travelMode: google.maps.TravelMode.DRIVING
+        }, (results: any) => {
+          this.tripDistance = (results.rows[0].elements[0].distance.value / 1000);
           this.tripTime = (results.rows[0].elements[0].duration.text);
           const pricePerRiderPerKm = 37;
           const tripPricePerRider = Math.round(pricePerRiderPerKm * this.tripDistance);
           this.tripPricePerRider = tripPricePerRider;
           console.log('trip price per rider', tripPricePerRider);
+          this.loading = false;
         });
+      }, 2000);
+    }
   }
 
   updateTrip() {
-    
+
   }
   computeTripFare(value) {
     const tripFare = value * this.tripPricePerRider;
     this.fare = tripFare;
     this.tripForm.patchValue({
-      aggregrateTripFee:  this.fare
+      aggregrateTripFee: this.fare
     });
     console.log(tripFare);
   }
@@ -119,19 +149,19 @@ export class DriverTripCreateComponent implements OnInit, OnDestroy {
       const maxRiderNumber = this.tripForm.value.maxRiderNumber;
       if (this.tripForm.valid && maxRiderNumber <= seatCapacity) {
         this.activeTripService.createTrip(this.tripForm.value)
-        .pipe(takeUntil(this.unsubscribe$))
-        .subscribe(trip =>  {
-          this.activeTrip = trip;
-          localStorage.setItem('activeTrip', JSON.stringify(this.activeTrip));
-          const message = 'Your trip has been created and is currently being broadcasted';
-          this.notifyService.showSuccessMessage(message);
-          this.router.navigate(['driver/rider-request']);
-        }, error => {
-          setTimeout(() => {
-            this.loading = false;
-          }, 3000);
-        });
-      } else if (this.tripForm.value.tripStartDateTime ) {
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe(trip => {
+            this.activeTrip = trip;
+            localStorage.setItem('activeTrip', JSON.stringify(this.activeTrip));
+            const message = 'Your trip has been created and is currently being broadcasted';
+            this.notifyService.showSuccessMessage(message);
+            this.router.navigate(['driver/rider-request']);
+          }, error => {
+            setTimeout(() => {
+              this.loading = false;
+            }, 3000);
+          });
+      } else if (this.tripForm.value.tripStartDateTime) {
 
       } else {
         const message = 'number of riders exceeds car capacity';
