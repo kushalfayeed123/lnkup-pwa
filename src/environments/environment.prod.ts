ec2690dd14eb900f56190546fb4aeaaed933b5c2
King Kush <segunajanaku617@gmail.com>
2020-03-08 01:50:53 +0100

    change backend endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  webUrl : 'https://linkup20191021111853.azurewebsites.net/api',
+  // webUrl : 'https://linkup20191021111853.azurewebsites.net/api',
+  webUrl: 'https://linkup20200307092355.azurewebsites.net/api',
   openConnect: 'https://linkup20191021111853.azurewebsites.net/notifyHub',
   vapidPublicKey: 'BCleJ0xgtXBcFHep4RmZXWX3Fa6S6ofO-XCzK56AHW3CXPlXZBsOtQ1ydmoidDBXaPeZdvA70ybSXB6atJBq7Jc',
   ravePubKey: 'FLWPUBK-739886009b82a2388b326c76226bad2b-X',
