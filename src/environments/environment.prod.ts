ffbc55569f4bb0ded5dfc70ba73b3110d770696f
King Kush <segunajanaku617@gmail.com>
2020-03-14 19:17:22 +0100

    change signal r endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  // webUrl : 'https://linkup20191021111853.azurewebsites.net/api',
   webUrl: 'https://linkup20200307092355.azurewebsites.net/api',
-  openConnect: 'https://linkup20191021111853.azurewebsites.net/notifyHub',
+  openConnect: 'https://linkup20200307092355.azurewebsites.net/notifyHub',
   vapidPublicKey: 'BCleJ0xgtXBcFHep4RmZXWX3Fa6S6ofO-XCzK56AHW3CXPlXZBsOtQ1ydmoidDBXaPeZdvA70ybSXB6atJBq7Jc',
   ravePubKey: 'FLWPUBK-739886009b82a2388b326c76226bad2b-X',
   raveEndpoint: 'https://api.ravepay.co/flwv3-pug/getpaidx/api/charge',
@@ -14,5 +13,7 @@ export const environment = {
   // ravePubKey: 'FLWPUBK_TEST-de83d2331a09f1d56894a397f1aab8ec-X',
   // raveEndpoint: 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/charge',
   // raveValidateEndpoint: 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/validatecharge'
+  // webUrl : 'https://linkup20191021111853.azurewebsites.net/api',
+  // openConnect: 'https://linkup20191021111853.azurewebsites.net/notifyHub',
 
 };
