a7bbf5399af3b4b90e486582d397ac3a30544da0
Oluwasegun Ajanaku <sajanaku@eminenttechnology.com>
2020-04-04 17:59:46 +0100

    get latest

--- src/app/components/support/support.component.ts
+++ src/app/components/support/support.component.ts
@@ -12,8 +12,8 @@ import { slideInAnimation } from 'src/app/services/misc/animation';
   selector: 'app-support',
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.scss'],
-  animations: [slideInAnimation],
-  host: { '[@slideInAnimation]': '' }
+  // animations: [slideInAnimation],
+  // host: { '[@slideInAnimation]': '' }
 })
 export class SupportComponent implements OnInit, OnDestroy, AfterViewInit {
 
@@ -37,19 +37,20 @@ export class SupportComponent implements OnInit, OnDestroy, AfterViewInit {
   isMe: boolean;
   groupName: string;
   sent: any;
+  loading: boolean;
   constructor(private _router: Router, private fb: FormBuilder,
-              private reviewService: AppReviewDataService,
-              private route: ActivatedRoute,
-              private notifyService: NotificationsService) {
+    private reviewService: AppReviewDataService,
+    private route: ActivatedRoute,
+    private notifyService: NotificationsService) {
   }
 
   ngOnInit() {
     this.notifyService.intiateConnection();
     this.route.queryParams
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe(param => {
-      this.reviewType = param.reviewType;
-    });
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(param => {
+        this.reviewType = param.reviewType;
+      });
     this.messageForm = this.fb.group({
       userReview: ['', [Validators.required]],
       userId: ['', [Validators.required]],
@@ -66,20 +67,23 @@ export class SupportComponent implements OnInit, OnDestroy, AfterViewInit {
       this.receiveGroupMessage();
     }, 5000);
     // this.scroll.nativeElement.scrollTo(0, this.scroll.nativeElement.scrollHeight);
- }
+  }
 
   addToGroup() {
+    this.loading = true;
     this.groupName = localStorage.getItem('groupName');
     this.notifyService.addUserToGroup(this.groupName);
     this.notifyService.sentFlag
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe(res => {
-      if (!res) {
-        return;
-      } else {
-        this.sent = res;
-      }
-    });
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(res => {
+        if (!res) {
+          this.loading = false;
+          return;
+        } else {
+          this.loading = false;
+          this.sent = res;
+        }
+      });
   }
 
   getCurrentUser() {
@@ -147,28 +151,28 @@ export class SupportComponent implements OnInit, OnDestroy, AfterViewInit {
   sendMessage() {
     if (this.reviewType === 'comment') {
       this.getCurrentTime();
-    this.message = this.messageForm.value.userReview;
-    // this.allMessages = [...this.allMessages, this.message];
-    this.messageForm.patchValue({userId: this.userId, reviewTime: this.currentDateTime, reviewType: this.reviewType, userRating: 0});
-    if (this.messageForm.valid) {
-      console.log(this.reviewType)
-      this.reviewService.createReview(this.messageForm.value)
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(res => {
-        if (res) {
-          this.messageForm.reset();
-          this.notifyService.showSuccessMessage('Thank you for using LnkuP. Your review has been submitted, we will get back to you as soon as possible.')
-        } else {
-          return;
-        }
-      }, err => {
-        this.notifyService.showErrorMessage('We could not submit your comment, this might be due to a network error please try again')
-      });
-    }
+      this.message = this.messageForm.value.userReview;
+      // this.allMessages = [...this.allMessages, this.message];
+      this.messageForm.patchValue({ userId: this.userId, reviewTime: this.currentDateTime, reviewType: this.reviewType, userRating: 0 });
+      if (this.messageForm.valid) {
+        console.log(this.reviewType)
+        this.reviewService.createReview(this.messageForm.value)
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe(res => {
+            if (res) {
+              this.messageForm.reset();
+              this.notifyService.showSuccessMessage('Thank you for using LnkuP. Your review has been submitted, we will get back to you as soon as possible.')
+            } else {
+              return;
+            }
+          }, err => {
+            this.notifyService.showErrorMessage('We could not submit your comment, this might be due to a network error please try again')
+          });
+      }
     } else {
       this.sendGroupMessage(this.messageForm.value.userReview);
     }
-    
+
   }
 
   ngOnDestroy() {
